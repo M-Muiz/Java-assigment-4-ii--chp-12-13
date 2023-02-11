26c063be056cb78368349cac4b9245f9350c122d
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-02-11 15:15:40 +0500

    12-13

---
+++ js/java.js
@@ -0,0 +1,98 @@
+Q:1
+
+// var character = prompt("");
+//   if (character == character.toUpperCase()) {
+// alert ('upper case ');
+//  }
+//  if (character == character.toLowerCase()){
+//   alert ('lower case ');
+// }
+
+Q:2
+
+// let num1 = +prompt("Enter Num 1")
+// let num2 = +prompt("Enter Num 2")
+
+// if(num1 >num2){
+//     alert("num1")
+
+// }
+
+// else if(num2 >num1){
+//     alert("num2")
+// }
+
+// else if(num1 === num2){
+// alert("Both are equal")
+// }
+
+Q:3
+
+// let num =+prompt("");
+
+// if(num <= 0){
+// alert("Negative")
+// }
+
+// else if(num ===0){
+// alert("Zero")
+// }
+
+// else if(num >= 0){
+// alert("Positive")
+// }
+
+q:4
+
+// let char = prompt("");
+
+// if(char =="a" || char =="e" || char =="i" || char =="o" || char =="u"){
+//     alert("Vowel")
+// }
+
+// else{
+//     alert("Not vowel")
+// }
+
+Q:5
+
+// let pass = prompt("Enter your password");
+
+// var orignal = ("moiz");
+
+// if(pass == orignal){
+//     alert("Correct password!")
+// }
+
+// else{
+//     alert ("Incorrect password")
+// }
+
+Q:6
+
+// let greet = prompt("");
+// if(greet >18){
+//     alert("Good Morning")
+// }
+
+// else{
+//     alert("Good Evening")
+// }
+
+Q:7
+
+// let time = +prompt("Enter Time");
+//  if (time >=00 && time <=12){
+//     alert("Good Morning")
+//  }
+
+// else if (time >=12 && time <=17){
+//     alert("Good AfterNoon")
+//  }
+
+// else if (time >=17 && time <=21){
+//     alert("Good Evening")
+//  }
+// else if (time >=21 && time <=24){
+//     alert("Good Night")
+//  }
